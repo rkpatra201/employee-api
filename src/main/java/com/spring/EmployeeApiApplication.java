26c95acb83e5fd6633afb3f8b903acbd2c5c2139
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2019-11-11 01:57:12 +0530

    enabled cors

--- src/main/java/com/spring/EmployeeApiApplication.java
+++ src/main/java/com/spring/EmployeeApiApplication.java
@@ -2,10 +2,25 @@ package com.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class EmployeeApiApplication {
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+						.allowedHeaders("*");
+			}
+		};
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApiApplication.class, args);
 	}
